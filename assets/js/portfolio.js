41baf0e7b210086e2d5236f6d612a75df84b419c
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-06-01 21:58:19 -0700

    Portfolio Redesign

---
+++ assets/js/portfolio.js
@@ -0,0 +1,48 @@
+var cards = [
+    {
+        name:"",
+        css:"card-color1",
+        imgSrc:"",
+        href:"",
+        href:"",
+        description:""
+
+    },
+    {
+         name:"",
+        css:"card-color2",
+        imgSrc:"",
+        href:"",
+        description:""
+    },
+    {
+         name:"",
+        css:"card-color3",
+        imgSrc:"",
+        href:"",
+        description:""
+    },
+    {
+         name:"",
+        css:"card-color4",
+        imgSrc:"",
+        href:"",
+        description:""
+    },
+    {
+         name:"",
+        css:"card-color5",
+        imgSrc:"",
+        href:"",
+        description:""
+    },
+    {
+         name:"",
+        css:"card-color6",
+        imgSrc:"",
+        href:"",
+        description:""
+    }
+
+]
+

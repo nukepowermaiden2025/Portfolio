a8e66bec785081cde212d1008bcc744b76a2b0e0
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-06-01 23:14:14 -0700

    Adding the card objects

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,26 +1,25 @@
 var cards = [
     {
-        name:"",
+        name:"Giphy World",
         css:"card-color1",
-        imgSrc:"",
-        href:"",
-        href:"",
-        description:""
+        imgSrc:"https://media.giphy.com/media/Q8VKl8nT8UhFK/giphy.gif",
+        href:"https://nukepowermaiden2025.github.io/GiphyWeek6/",
+        description:"Controling gif state with AJAX and jquery."
 
     },
     {
-         name:"",
+        name:"Jarvis Health App",
         css:"card-color2",
-        imgSrc:"",
-        href:"",
-        description:""
+        imgSrc:"./assets/images/Jarvis.jpg",
+        href:"https://github.com/nukepowermaiden2025/Jarvis",
+        description:"Location based Health Analysis with MVC Design Pattern"
     },
     {
-         name:"",
+        name:"Nellie's Bucket List",
         css:"card-color3",
-        imgSrc:"",
-        href:"",
-        description:""
+        imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNvlgSeLHqTD1ZmYmYemcSakiZSo1VB6t8KVgMEnG8NJ-6sFDyhw",
+        href:"https://nellies-ultimate-bucketlist.herokuapp.com/",
+        description:"Creative BucketList with MySQL and Handlebars"
     },
     {
          name:"",
